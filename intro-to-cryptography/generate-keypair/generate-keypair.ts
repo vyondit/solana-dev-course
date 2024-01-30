3416cafe3abf28226601161af934ea525e433197
Mijo Safradin <mijo.safradin@vyondit.de>
2024-01-30 22:18:54 +0100

    Add checks to last lab example

--- intro-to-cryptography/generate-keypair/generate-keypair.ts
+++ intro-to-cryptography/generate-keypair/generate-keypair.ts
@@ -1,13 +1,14 @@
-import "dotenv/config"
-
+import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
+try {
+  const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
-const keypair = getKeypairFromEnvironment("SECRET_KEY");
+  if (!keypair) {
+    throw new Error("Keypair could not be loaded from environment variable.");
+  }
 
-
-console.log(
-
-  `✅ Finished! We've loaded our secret key securely, using an env file!`
-
-);
+  console.log(`✅ Finished! We've loaded our secret key securely, using an env file!`);
+} catch (error) {
+  console.error(`❌ Error: ${error.message}`);
+}
