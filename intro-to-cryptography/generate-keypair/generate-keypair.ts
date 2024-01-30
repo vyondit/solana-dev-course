071c6a4d4812e5bc018c6582e3b2d4bffd766935
Mijo Safradin <mijo.safradin@vyondit.de>
2024-01-30 22:07:37 +0100

    first example

--- intro-to-cryptography/generate-keypair/generate-keypair.ts
+++ intro-to-cryptography/generate-keypair/generate-keypair.ts
@@ -1,8 +1,9 @@
-import "dotenv/config"
-import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { Keypair } from "@solana/web3.js";
 
-const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
-console.log(
-  `✅ Finished! We've loaded our secret key securely, using an env file!`
-);
+const keypair = Keypair.generate();
+
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+
+console.log(`The secret key is: `, keypair.secretKey);
