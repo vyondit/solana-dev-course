c619894f343b9e5947e367f2c35471f34a0b888e
Mijo Safradin <mijo.safradin@vyondit.de>
2024-01-30 22:28:04 +0100

    Add environment dependent configuration

--- intro-to-cryptography/generate-keypair/generate-keypair.ts
+++ intro-to-cryptography/generate-keypair/generate-keypair.ts
@@ -1,5 +1,10 @@
-import "dotenv/config";
+import fs from 'fs';
+import * as dotenv from 'dotenv';
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const env = process.env.NODE_ENV || 'development';
+
+dotenv.config({ path: `.env.${env}` });
 
 try {
   const keypair = getKeypairFromEnvironment("SECRET_KEY");
